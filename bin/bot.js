cc8cee33e0c7b8aa299a8d6c28602169a9ec3e55
Devon Wesley <dwesley89@gmail.com>
2016-11-27 15:57:17 -0800

    created a bruce leroy bot that say a whole bunch of chuck norris jokes, have to find Bruce Leroy API

--- bin/bot.js
+++ bin/bot.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const BruceLeroy = require('../lib/bruceleroy')
 const token = process.env.BOT_API_KEY
 const dbPath = process.env.BOT_DB_PATH
 const name = process.env.BOT_NAME
-
 const bruceleroy = new BruceLeroy({
     token: token,
     dbPath: dbPath,
